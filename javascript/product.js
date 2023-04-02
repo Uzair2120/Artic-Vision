5c202f93817d88f7ad48f0bc427189d43d085e82
Uzair2120 <asnuzarshad2004@gmail.com>
2023-04-02 21:50:04 +0500

    bb

---
+++ javascript/product.js
@@ -0,0 +1,21 @@
+var nav = document.getElementById("main1");
+window.addEventListener('scroll', function (a) {
+    if (window.pageYOffset > 10) {
+        nav.style.boxShadow = "0 4px 5px -3px gray";
+    } else {
+        nav.style.boxShadow = "none";
+    }
+})
+
+
+var button = document.querySelector(".btn");
+var in_btn = document.querySelector(".in-btn");
+var menu = document.querySelector(".for-menu");
+button.addEventListener('click', function (a) {
+    button.classList.toggle("active-btn");
+    in_btn.classList.toggle("active-inbtn");
+    menu.classList.toggle("for-menu-on");
+
+})
+
+
